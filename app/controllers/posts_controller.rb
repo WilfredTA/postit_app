f9204a45950402a817c8ca5abc98893a133af5d9
Tannr Allard <rockinghamtannr@gmail.com>
2017-12-17 17:09:23 -0500

    Adding creating and editing post, as well as form submission validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,18 +8,39 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.creator = User.first
 
+    if @post.save
+      flash[:notice] = "Your post was created."
+      redirect_to posts_url
+    else
+      render :new
+    end
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
 
+    if @post.update(post_params)
+      flash[:notice] = "This post was successfully updated."
+      redirect_to post_url(@post)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
   end
 end
